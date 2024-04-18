08f0269e8816b882fb340072d5080cdb19b24d51
Ubludok <KomradUbludok@gmail.com>
2024-04-18 21:53:14 +0300

    Saiga+Sounds+fixed animations+reworked sprites+lua scripts

---
+++ Actors/MG/KordVoice.lua
@@ -0,0 +1,15 @@
+function Create(self)
+	self.WasSoundPlayed = false
+	self.SelectSound = CreateSoundContainer("Kord Select", "GFLRu.rte");
+end
+
+function Update(self)
+	self.controller = self:GetController();
+	if self:IsPlayerControlled() and self.WasSoundPlayed == false and self.SelectSound:IsBeingPlayed() == false then
+		self.SelectSound:Play(self.Pos)
+		self.WasSoundPlayed = true
+	end
+	if self:IsPlayerControlled() == false then
+		self.WasSoundPlayed = false
+	end
+end
