1fec47c68f82c252186bf1d02b58b9e770b6b371
Ubludok <KomradUbludok@gmail.com>
2024-06-07 08:42:51 +0300

    Changed variables in Evasion.lua

--- Actors/Evasion.lua
+++ Actors/Evasion.lua
@@ -1,8 +1,8 @@
 function Create(self)
 	self.EvasionTimer = Timer()
 	self.Evasion = self:NumberValueExists("Evasion") and self:GetNumberValue("Evasion")
-	self.AverageAccuracy = 50
-	self.TimeInterval = 250
+	self.AverageAccuracy = 70
+	self.TimeInterval = 500
 end
 
 function EvasionMath(acc, eva) 
@@ -11,18 +11,19 @@ end
 
 function Update(self)
 	local duration = math.random(self.TimeInterval)
+	print(EvasionMath(self.AverageAccuracy, self.Evasion))
 	if self.EvasionTimer:IsPastSimMS(duration) and math.random(100) <= EvasionMath(self.AverageAccuracy, self.Evasion)  then
-		--self:FlashWhite(1000)
-		self.EvasionTimer:Reset()
+		--self:FlashWhite(duration)
 		self.GetsHitByMOs = false;
 		for att in self.Attachables do
 			att.GetsHitByMOs = false;
 		end
-	elseif self.EvasionTimer:IsPastSimMS(duration) and math.random(100) > EvasionMath(self.AverageAccuracy, self.Evasion) then
 		self.EvasionTimer:Reset()
+	elseif self.EvasionTimer:IsPastSimMS(duration) and math.random(100) > EvasionMath(self.AverageAccuracy, self.Evasion) then
 		self.GetsHitByMOs = true;
 		for att in self.Attachables do
 			att.GetsHitByMOs = true;
 		end
+		self.EvasionTimer:Reset()
 	end
 end
